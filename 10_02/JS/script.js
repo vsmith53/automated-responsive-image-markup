a78de7a01eb1fc71f39644054575c3cd1f49c3a5
Viviana <vsmith53@alamo.edu>
2019-10-16 21:23:21 -0500

    second commit

---
+++ 10_02/JS/script.js
@@ -0,0 +1,33 @@
+const IMAGES = document.querySelectorAll("img");
+const SIZES = {
+	showcase: "100vw",
+	reason: "(max-width: 799px) 100vw, 372px",
+	feature: "(max-width: 799px) 100vw, 558px",
+	story: "(max-width: 799px) 100vw, 670px"
+}
+function makeSrcset(imgSrc) {
+	let markup = [];
+	let width = 400;
+
+	for (let i = 0; i<5; i++) {
+		markup[i] = imgSrc + "-" + width + ".jpg " + width + "w"
+		width+=400;
+
+	}
+
+	return markup.join();
+}
+
+for (let i = 0; i<IMAGES.length; i++) {
+	let imgSrc = IMAGES[i].getAttribute("src");
+	imgSrc = imgSrc.slice(0, -8);
+	let srcSet = makeSrcset(imgSrc);
+	console.log(srcSet);
+
+	let type = IMAGES[i].getAttribute("data-type");
+	let sizes = SIZES[type];
+	console.log(sizes);
+}
+
+
+
