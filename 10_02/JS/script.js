3cf3b78008640b85d8b53545e274cb3c4be9fb33
Viviana <vsmith53@alamo.edu>
2019-10-16 21:26:25 -0500

    final commit

--- 10_02/JS/script.js
+++ 10_02/JS/script.js
@@ -22,11 +22,13 @@ for (let i = 0; i<IMAGES.length; i++) {
 	let imgSrc = IMAGES[i].getAttribute("src");
 	imgSrc = imgSrc.slice(0, -8);
 	let srcSet = makeSrcset(imgSrc);
-	console.log(srcSet);
+	IMAGES[i].setAttribute("srcset", srcSet);
+	
 
 	let type = IMAGES[i].getAttribute("data-type");
 	let sizes = SIZES[type];
 	console.log(sizes);
+	IMAGES[i].setAttribute("sizes", sizes);
 }
 
 
